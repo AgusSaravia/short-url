d84d6bd847385a244eb0213d9cb147ae089b14cf
AgusSaravia <agustinsaravia268@gmail.com>
2024-10-12 08:46:27 -0300

    Changed previous algorithm for creating short urls  to uuid

--- src/utils/getUniqueIdentifier.js
+++ src/utils/getUniqueIdentifier.js
@@ -1,10 +1,5 @@
-const crypto = require("node:crypto");
+const {v4: uuidv4} = require("uuid");
 
 exports.getUniqueIdentifier = () => {
-  const randomBytes = crypto.randomBytes(4).toString("hex");
-  const timestamp = Date.now().toString();
-  const uniqueEntry = randomBytes + timestamp;
-  const id = uniqueEntry.substr(0, 7);
-
-  return id;
+    return  uuidv4().substr(0, 7);
 };
